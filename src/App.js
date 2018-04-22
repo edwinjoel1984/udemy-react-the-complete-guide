8ddf860128a669961e2bbde3474da3071c3804d7
Edwin Ulloa <edwinjoel1984@gmail.com>
2018-04-22 17:15:56 -0500

    Finish Working with Lists and Conditionals

--- src/App.js
+++ src/App.js
@@ -5,27 +5,33 @@ import Person from './Components/Person/Person';
 class App extends Component {
   state={
     persons : [
-      {name : 'Max', age: 25},
-      {name : 'Manu', age: 22},
-      {name : 'Eliana', age: 15}
+      { id:'1',name : 'Max', age: 25},
+      { id:'2',name : 'Manu', age: 22},
+      { id:'3',name : 'Eliana', age: 15}
     ],
     showPersons: false
   }
   paintPerson = (person,index) =>{
-    console.log(person,index)
-    return  <Person key={index} name={person.name} age={person.age}
-              changed={this.changeNameHandler}
+    return  <Person key={person.id} name={person.name} age={person.age}
+              changed={(event) => this.changeNameHandler(event,person.id)}
+              click ={()=>this.deletePersonHandler(index)}
     >{person.children}</Person>
   }
-  changeNameHandler = (event) => {
+  changeNameHandler = (event, id) => {
     console.log(event)
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id===id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    person.name=event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState (
       {
-      persons : [
-        {name : 'Max', age: 251},
-        {name : event.target.value, age: 222},
-        {name : 'Eliana', age: 151, children: 'I am the children'}
-      ]
+      persons
     }
   )
   }
@@ -41,6 +47,13 @@ class App extends Component {
         ]
       }
     )
+  }
+
+  deletePersonHandler = (personIndex)=>{
+    // const { persons } = this.state;
+    const persons = [...this.state.persons];
+    persons.splice(personIndex,1);
+    this.setState({persons})
   }
 
   tooglePersonsHandler = () =>{
--- src/Components/Person/Person.js
+++ src/Components/Person/Person.js
@@ -8,6 +8,7 @@ const Person = (props) => {
                     <h2>My name is {props.name} and i'm {props.age} years old</h2>
                     <input type="text" onChange={props.changed} value={props.name}/>
                     <h3>{props.children}</h3>
+                    <button onClick={props.click}>Remove user</button>      
                 </div>
         )
 }
