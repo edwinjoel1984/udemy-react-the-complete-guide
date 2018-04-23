d3b63aa122353281edb21a389a80769051aa2e28
Edwin Ulloa Silva <edwinjoel1984@gmail.com>
2018-04-22 21:07:50 -0500

    Finish

--- src/Components/Person/Person.js
+++ src/Components/Person/Person.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Radium from 'radium';
 
-import './Person.css';
+import classes from './Person.css';
 
 const Person = (props) => {
     
         return (
-                <div className="Person">
+                <div className={classes.Person}>
                     <h2>My name is {props.name} and i'm {props.age} years old</h2>
                     <input type="text" onChange={props.changed} value={props.name}/>
                     <h3>{props.children}</h3>
